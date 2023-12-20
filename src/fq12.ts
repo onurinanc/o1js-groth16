e7d86cd2dbd6809989677c5220e5c7890dc31940
Onur İnanç <e191322@metu.edu.tr>
2023-12-20 14:05:52 +0300

    add invert and cyclotomic square

--- src/fq12.ts
+++ src/fq12.ts
@@ -138,5 +138,103 @@ export default class Fq12{
             c1_new
         );
     }
+
+    invert() {
+        let c0s = this.c0;
+        c0s = c0s.square();
+
+        let c1s = this.c1;
+        c1s = c1s.square();
+        c1s = c1s.mul_by_nonresidue();
+        c0s = c0s.sub(c1s);
+
+        let t = c0s.invert();
+        let c0 = t.mul(this.c0); // check this
+        let c1 = t.mul(this.c1); // check this
+        c1 = c1.neg();
+
+        return new Fq12(
+            c0,
+            c1
+        );
+
+    }
+
+    // instead of transforming
+    // let's return c0 of Fq6 and c1 of Fq6
+    // we can change Fq2.zero() later
+    fp4_square(a0: Fq2, a1: Fq2) {
+        let t0 = a0.square();
+        let t1 = a1.square();
+        let t2 = t1;
+        t2 = t2.mul_by_nonresidue();
+        let c0 = t2.add(t0);
+        t2 = a0.add(a1);
+        t2 = t2.square();
+        t2 = t2.sub(t0);
+        let c1 = t2.sub(t1);
+
+        return new Fq6(
+            c0,
+            c1,
+            Fq2.zero()
+        );
+    }
+
+    cyclotomic_square() {
+        let t3_t4 = this.fp4_square(this.c0.c0, this.c1.c1);
+        let t3 = t3_t4.c0;
+        let t4 = t3_t4.c1;
+
+        let t2 = t3.sub(this.c0.c0);
+        t2 = t2.double();
+        
+        let new_c0_c0 = t2.add(t3);
+
+        t2 = t4.add(this.c1.c1);
+        t2 = t2.double();
+
+        let new_c1_c1 = t2.add(t4);
+
+        let new_t3_t4 = this.fp4_square(this.c1.c0, this.c0.c2);
+
+        t3 = new_t3_t4.c0; // Check this
+        t4 = new_t3_t4.c1; // Check this
+
+        let t5_t6 = this.fp4_square(this.c0.c1, this.c1.c2);
+
+        let t5 = t5_t6.c0;
+        let t6 = t5_t6.c1;
+
+        t2 = t3.sub(this.c0.c1);
+        t2 = t2.double();
+
+        let new_c0_c1 = t2.add(t3);
+        t2 = t4.add(this.c1.c2);
+        t2 = t2.double();
+
+        let new_c1_c2 = t2.add(t4);
+        t3 = t6;
+        t3 = t3.mul_by_nonresidue();
+        t2 = t3.add(this.c1.c0);
+        t2 = t2.double();
+        let new_c1_c0 = t2.add(t3);
+        t2 = t5.sub(this.c0.c2);
+        t2 = t2.double();
+        let new_c0_c2 = t2.add(t5);
+
+        return new Fq12(
+            new Fq6(
+                new_c0_c0,
+                new_c0_c1,
+                new_c0_c2
+            ),
+            new Fq6(
+                new_c1_c0,
+                new_c1_c1,
+                new_c1_c2
+            )
+        );
+    }
 }
 
